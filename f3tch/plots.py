2572f924135b9275376243eab495c306ff396f24
Sharat Akhoury <sakhoury@redhat.com>
2022-10-05 15:33:31 -0400

    Address pylint warnings
    Signed-off-by: Sharat Akhoury <sakhoury@redhat.com>

--- f3tch/plots.py
+++ f3tch/plots.py
@@ -9,6 +9,9 @@ import numpy as np
 # custom imports
 from f3tch import utils
 
+# pylint: disable=too-many-arguments
+# pylint: disable-msg=too-many-locals
+
 
 def plot_timeseries(time_series, metric_name, moving_avg_window_size=0, time_slices=None,
                     plot_title="Time Series Plot", plot_minmax=False, plot_filename="",
@@ -69,21 +72,21 @@ def plot_timeseries(time_series, metric_name, moving_avg_window_size=0, time_sli
             tmp_df = tmp_df.loc[tmp_df.index >= y_vals.index.values[0]]
             lbl = time_slice["label"]
             axis.plot(tmp_df, label=f"{lbl}_{metric_name_lbl}",
-                    color=time_slice['color'])
+                      color=time_slice['color'])
 
     # Plot moving average
     if moving_avg_window_size > 0:
         axis.plot(
             vals_avg, label=f"moving_avg_{metric_name_lbl}", color='orchid')
         axis.plot(vals_avg+vals_stdev, ':', label=f"moving_stdev_{metric_name_lbl}",
-                color='crimson')
+                  color='crimson')
         axis.plot(vals_avg-vals_stdev, ':', color='crimson')
 
         if plot_minmax:
             axis.plot(vals_min, label=f"moving_min_{metric_name_lbl}", color='darkturquoise',
-                    alpha=0.4)
+                      alpha=0.4)
             axis.plot(vals_max, label=f"moving_max_{metric_name_lbl}", color='orangered',
-                    alpha=0.4)
+                      alpha=0.4)
     pyplot.legend()
     if plot_filename != "":
         pyplot.savefig(plot_filename, bbox_inches='tight', dpi=300)
@@ -158,11 +161,10 @@ def process_time_slice_data(time_series, metric_name, time_slices, moving_avg_wi
         tmp_df = utils.date_filter(
             time_series_df=vals, date_range=time_slice['time_range'])
         lbl = time_slice["label"]
-        yy = tmp_df.to_numpy()
-        xx = np.asarray(tmp_df.index, dtype='datetime64[m]')
 
-        data.append({"x": xx, "y": yy, "lbl": f"{lbl}_{metric_name_lbl}",
-                    'color': time_slice['color']})
+        data.append({"x": np.asarray(tmp_df.index, dtype='datetime64[m]'),
+                     "y": tmp_df.to_numpy(), "lbl": f"{lbl}_{metric_name_lbl}",
+                     "color": time_slice['color']})
 
     return data
 
@@ -212,7 +214,7 @@ def plot_time_slices_overlaid(time_series, metric_name, time_slices, moving_avg_
 def plot_time_slices_discontiguous(time_series, metric_name, time_slices, moving_avg_window_size=0,
                                    plot_title="Time Series Plot", plot_filename="", x_spacing=20,
                                    x_tick_rotation=70, verbose=False):
-    """Function to plot the time-slices data in discreet time-space only using matplotlib.pyplot 
+    """Function to plot the time-slices data in discreet time-space only using matplotlib.pyplot
     library
 
     Args:
@@ -254,11 +256,12 @@ def plot_time_slices_discontiguous(time_series, metric_name, time_slices, moving
             _xticks.append(_data["x"][k])
 
         axis.plot(range(start, end), _data["y"],
-                label=_data["lbl"], color=_data["color"])
+                  label=_data["lbl"], color=_data["color"])
 
     ticks = range(0, len(_xticks))
     num_samples = np.min([10, math.floor(math.sqrt(len(_xticks)))])
-    sample_idx = get_sample_idx(num_indices=len(_xticks), num_samples=num_samples)
+    sample_idx = get_sample_idx(
+        num_indices=len(_xticks), num_samples=num_samples)
 
     axis.set_xticks([ticks[idx] for idx in sample_idx])
     axis.set_xticklabels([_xticks[idx] for idx in sample_idx])
